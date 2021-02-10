8b6239148e83fc02db7791e2babc7ca6a3b036b3
Julie Mahe <j-mahe@hotmail.fr>
2021-02-10 16:34:07 +0100

    try to do offers on Home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./assets/scss/style.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Offer from "./containers/Offer";
 import Home from "./containers/Home";
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,43 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 const axios = require("axios");
 
 const Home = () => {
-  axios
-    .get("https://lereacteur-vinted-api.herokuapp.com/offers")
-    .then((response) => {});
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, [data]);
 
   return (
     <div className="home">
-      <div>IMG GIGANTESQUE</div>
-      <div>{response.data.offers}</div>
+      {loading ? (
+        <span>en cours de chargement...</span>
+      ) : (
+        <div>
+          <div>IMG GIGANTESQUE</div>
+          {data.offers.map((elem, index) => {
+            console.log(elem.product_pictures[index].secure_url);
+            return (
+              <div key={elem._id}>
+                <img src={elem.product_pictures[0].secure_url} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
