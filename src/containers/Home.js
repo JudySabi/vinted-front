5a25a33411063a7b9c0680516b6dd5fca5face9f
Julie Mahe <j-mahe@hotmail.fr>
2021-02-10 22:37:30 +0100

    finish routes home & offer

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -23,25 +23,33 @@ const Home = () => {
 
   return (
     <div className="home">
+      {/* <div>IMG GIGANTESQUE</div> */}
       {loading ? (
         <span>en cours de chargement...</span>
       ) : (
-        <div>
-          <div>IMG GIGANTESQUE</div>
+        <>
           {data.offers.map((elem) => {
+            // je récupère la marque du tableau product details pour pouvoir l'afficher plus bas
+            const marque = Object.values(elem.product_details[0]);
+            console.log(marque);
             return (
-              <Link elem={elem} to={`/offer/${elem._id}`}>
-                <div key={elem._id}>
-                  {elem.owner.account.avatar && (
-                    <img src={elem.owner.account.avatar.secure_url} alt="" />
-                  )}
+              <Link to={`/offer/${elem._id}`}>
+                <div key={elem._id} className="offerCard">
+                  <div className="user">
+                    {elem.owner.account.avatar && (
+                      <img src={elem.owner.account.avatar.secure_url} alt="" />
+                    )}
+                    <p>{elem.owner.account.username}</p>
+                  </div>
 
                   <img src={elem.product_image.secure_url} alt="" />
+                  <p>{elem.product_price} €</p>
+                  <p>{marque}</p>
                 </div>
               </Link>
             );
           })}
-        </div>
+        </>
       )}
     </div>
   );
--- src/containers/Offer.js
+++ src/containers/Offer.js
@@ -22,7 +22,7 @@ const Offer = () => {
     };
     fetchData();
   }, []);
-  console.log(data);
+
   return (
     <>
       {loading ? (
@@ -30,14 +30,34 @@ const Offer = () => {
       ) : (
         <div className="offer">
           <img src={data.product_image.secure_url} alt="" />
-          <div>
-            <div>{data.product_price}</div>
-            <div>{data.product_name}</div>
+          <div className="card">
+            <div className="topCard">
+              <p>{data.product_price} €</p>
+              {/* AFFICHE MES PRODUCTS_DETAILS : */}
+              {data.product_details.map((elem, index) => {
+                const keys = Object.keys(elem);
+                const elems = Object.values(elem);
+                return (
+                  <div key={index}>
+                    <span>{keys}</span>
+                    <span>{elems}</span>
+                  </div>
+                );
+              })}
+            </div>
 
-            <div>{data.product_details[0].MARQUE}</div>
-            <div>{data.product_details[1].ÉTAT}</div>
-            <div>{data.product_details[2].COULEUR}</div>
-            <div>{data.product_details[3].EMPLACEMENT}</div>
+            <div className="divider"></div>
+
+            <div className="bottomCard">
+              <h1>{data.product_name}</h1>
+              <p>{data.product_description}</p>
+
+              <div className="user">
+                <img src={data.owner.account.avatar.secure_url} alt="" />
+                <p>{data.owner.account.username}</p>
+              </div>
+              <button>Acheter</button>
+            </div>
           </div>
         </div>
       )}
